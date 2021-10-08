dd6e3a258eb6c102ebbbc23955cfc77db8166456
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-10-08 19:02:17 +0600

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react'
+import { increment, decrement, incrementByAmount } from './redux/counter';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [launches, setLaunches] = useState([]);
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/launches').then(res => res.json()).then(data => setLaunches([...data]))
+  }, [])
+
   return (
     <div className="App">
       
-        <h1 className='text-xl font-semibold'>the count is: {count}</h1>
-
-        <button className='border text-sm p-2' onClick={() => setCount(count+1)}>Increment</button>
-        <button className='border text-sm p-2' onClick={() => setCount(count-1)}>Decrement</button>
-      
+        <h1 className='text-4xl font-semibold'>the count is: {count}</h1>
+        <button className='border text-sm p-2' onClick={() => dispatch(increment())}>Increment</button>
+        <button className='border text-sm p-2' onClick={() => dispatch(decrement())}>Decrement</button>
+        <button className='border text-sm p-2' onClick={() => dispatch(incrementByAmount(33))}>Increment by 33</button>
+        {launches.map(launch => (
+          <p key={launch.flight_number}>{launch.mission_name}</p>
+        ))}
     </div>
   );
 }
