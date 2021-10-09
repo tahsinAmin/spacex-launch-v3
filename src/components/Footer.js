a614a27a3e2f7a62465edf2d27bf77784a272361
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-10-09 12:18:33 +0600

    Populating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
    return (
-      <div>
-         Footer
+      <div className='footer bg-gray-400 h-40 flex justify-center items-center text-black flex-col'>
+         <div>SpaceX</div>
+         <div>(C)2021, SpaceX, Inc. or its affiliates</div>
       </div>
    )
 }
